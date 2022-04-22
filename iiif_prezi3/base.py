8290dc358776b2fc2d7ede2cbb224f176da8de8d
Mike Bennett <mike.bennett@ed.ac.uk>
2022-04-22 14:09:44 +0100

    Add jsonld_dict method to Base class

--- iiif_prezi3/base.py
+++ iiif_prezi3/base.py
@@ -4,8 +4,7 @@ import json
 class Base(BaseModel):
     class Config:
          validate_assignment = True
-        #  extra = Extra.allow
-
+        #  extra = Extra.forbid
     def __getattribute__(self, prop):
         val = super(Base, self).__getattribute__(prop)
         # __root__ is a custom pydantic thing 
@@ -28,7 +27,7 @@ class Base(BaseModel):
         # return '{"@context": "http://iiif.io/api/presentation/3/context.json",' + json_str[1:]
 
         ### approach 4 - use the pydantic .dict() function to get the dict, add the context at the top and dump it to json - fails because you can't pass the pydantic arguments so get all the null values
-        #return json.dumps({"@context": "http://iiif.io/api/presentation/3/context.json", **self.dict()}, **kwargs)
+        # return json.dumps({"@context": "http://iiif.io/api/presentation/3/context.json", **self.dict()}, **kwargs)
 
         ### approach 5 - use pydantic to render json, reparse it, add the context and re-dump with modified kwargs - works but it's a bit messy
         # new_dict = {"@context": "http://iiif.io/api/presentation/3/context.json", **json.loads(self.json(**kwargs))}
@@ -46,4 +45,8 @@ class Base(BaseModel):
         json_kwargs = dict([(arg, kwargs[arg]) for arg in kwargs.keys() if arg not in pydantic_args])
         return json.dumps({"@context": "http://iiif.io/api/presentation/3/context.json", **self.dict(**dict_kwargs)}, **json_kwargs)
 
-       
+    
+    def jsonld_dict(self, **kwargs):
+        pydantic_args = ["include", "exclude", "by_alias", "exclude_unset", "exclude_defaults", "exclude_none", "encoder"]
+        dict_kwargs = dict([(arg, kwargs[arg]) for arg in kwargs.keys() if arg in pydantic_args])
+        return {"@context": "http://iiif.io/api/presentation/3/context.json", **self.dict(**dict_kwargs)}
